921d5ddde6e965e6ff27977e951777b227ef3f4b
jeff5m <jeff.matheus18@gmail.com>
2022-08-19 08:24:54 -0300

    feat: serving static html

--- 01-github-explorer/webpack.config.js
+++ 01-github-explorer/webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path') // importar usando required pois o webpack roda no node e o node entede apenas esse formato
+const HtmlWebpackPlugin = require('html-webpack-plugin')
 
 module.exports = {
     mode: 'development',
@@ -10,6 +11,11 @@ module.exports = {
     resolve: {
         extensions: ['.js', '.jsx'],
     },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: path.resolve(__dirname, 'public', 'index.html')
+        })
+    ],
     module: {
         rules: [
             {
