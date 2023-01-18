025fd5aacc03c17d0e7348ca5cde8822ac59d93f
Jeff Pereira <jeff.pereira@quintoandar.com.br>
2023-01-18 17:54:11 -0300

    Component: Sidebar

--- 01-fundamentos-reactjs/src/App.jsx
+++ 01-fundamentos-reactjs/src/App.jsx
@@ -1,22 +1,28 @@
 import { Header } from "./Components/Header";
 import { Post } from "./Post";
 
+import styles from "./App.module.css";
 import "./global.css";
+import { Sidebar } from "./Components/Sidebar";
 
 export function App() {
   return (
     <div>
       <Header />
-
-      <Post
-        author="Jeff Lee"
-        content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Itaque, autem."
-      />{" "}
-      <Post
-        author="Sid Man"
-        content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit repellendus quae fuga officiis ut minima temporibus animi, facilis aut, voluptatibus deleniti rem rerum! Repellat adipisci quos, rem expedita ad vitae?"
-      />
-      <Post />
+      <div className={styles.wrapper}>
+        <Sidebar />
+        <main>
+          <Post
+            author="Jeff Lee"
+            content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Itaque, autem."
+          />{" "}
+          <Post
+            author="Sid Man"
+            content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit repellendus quae fuga officiis ut minima temporibus animi, facilis aut, voluptatibus deleniti rem rerum! Repellat adipisci quos, rem expedita ad vitae?"
+          />
+          <Post />
+        </main>
+      </div>
     </div>
   );
 }
